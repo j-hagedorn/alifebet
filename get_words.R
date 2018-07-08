bc59d584b0e03f4edc7e53ff476c7810eafa6f7e
Joshh <j-hagedorn@users.noreply.github.com>
2018-07-08 17:52:57 -0400

    Add max and min len args

--- get_words.R
+++ get_words.R
@@ -9,17 +9,21 @@ get_words <- function(
   # The seed is different for different patterns: 
   # `letter_proportion` = letter, `vocal_proportion` = fricatives, etc., `cluster` = word to match, etc.
   seed_with,
-  prop = NULL
+  prop = NULL,
+  min_len = 1,
+  max_len = 20
 ){
   # Assumes a dataframe `lexicon` with the necessary columns
+  df <- lexicon %>% filter(between(len,min_len,max_len))
+  
   if (similarity %in% c("letter_proportion","vocal_proportion")){
     filter_criteria <- lazyeval::interp(~x >= y, .values = list(x = as.name(seed_with), y = prop))
-    return_words <- lexicon %>% filter_(filter_criteria) %>% droplevels() %>% dplyr::select(word) %>% c()
+    return_words <- df %>% filter_(filter_criteria) %>% droplevels() %>% dplyr::select(word) %>% c()
   } else if (similarity == "cluster") {
-    seed_clust <- lexicon %>% filter(word == seed_with) %>% dplyr::select(k_group) %>% c() 
-    return_words <- lexicon %>% filter(k_group == seed_clust[[1]]) %>% droplevels() %>% dplyr::select(word) %>% c()
+    seed_clust <- df %>% filter(word == seed_with) %>% dplyr::select(k_group) %>% c() 
+    return_words <- df %>% filter(k_group == seed_clust[[1]]) %>% droplevels() %>% dplyr::select(word) %>% c()
   } else if (similarity == "regex"){
-    return_words <- lexicon %>% filter(str_detect(word,seed_with) == T) %>% droplevels() %>% dplyr::select(word) %>% c()
+    return_words <- df %>% filter(str_detect(word,seed_with) == T) %>% droplevels() %>% dplyr::select(word) %>% c()
   } else if (similarity == "rhyme"){
     return_words <- c("Non-functional argument")
   } else if (similarity == "category"){
